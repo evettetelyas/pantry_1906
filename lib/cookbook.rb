80b7f26e1ceb61579fee54a2efe523cf41b166d2
Evette Telyas <etelyas@gmail.com>
2019-07-30 10:08:31 -0600

    work on summary hash

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -10,7 +10,15 @@ class Cookbook
   end
 
   def summary
-    
+    hash = Hash.new({})
+    @recipes.each do |recipe|
+      hash= Hash.new
+      hash[:name] = recipe.name
+      hash[:details] = Hash.new({})
+      hash[:details][:ingredients] = "ingredients go here?"
+      hash[:details][:total_calories] = recipe.total_calories
+      binding.pry
+    end
   end
 
 end
